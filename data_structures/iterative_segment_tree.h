793a8461d745cb5308d97aa72c366d139bdd7e69
Javier Oliva <jav.oliva.silva@gmail.com>
2021-10-12 12:06:25 -0300

    add comments

--- data_structures/iterative_segment_tree.h
+++ data_structures/iterative_segment_tree.h
@@ -5,12 +5,12 @@ template<class T> struct IterativeSegmentTree{
         for (int i=n;i<(n<<1);i++)ST[i]=a[i-n];
         for (int i=n-1;i>0;i--)ST[i]=m_(ST[i<<1],ST[i<<1|1]);
     }
-    void update(int pos, T val){
+    void update(int pos, T val){ // replace with val
         ST[pos += n] = val;
         for (pos >>= 1; pos > 0; pos >>= 1)
             ST[pos] = m_(ST[pos<<1], ST[pos<<1|1]);
     }
-    T query(int l, int r){
+    T query(int l, int r){ // [l, r]
         T ansL, ansR; bool hasL = 0, hasR = 0;
         for (l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
             if (l & 1) 
@@ -21,4 +21,4 @@ template<class T> struct IterativeSegmentTree{
         if (!hasL) return ansR; if (!hasR) return ansL;
         return m_(ansL, ansR);
     }
-};
+}; // Give vector of leaves and merge function
