d71b8207318a7d410d133c433ab91586fb88b16c
JavOliva <jav.oliva.silva@gmail.com>
2021-10-12 11:43:22 -0300

    iterative seg tree with less lines

--- data_structures/iterative_segment_tree.h
+++ data_structures/iterative_segment_tree.h
@@ -1,11 +1,8 @@
 template<class T>
 struct IterativeSegmentTree{
-    vector<T> ST;
-    T (*merge)(T, T);
-    int n;
+    int n; vector<T> ST; T (*merge)(T, T);
     IterativeSegmentTree(vector<T> &a, T (*merge)(T, T)) : merge(merge){
-        n = a.size();
-        ST.resize(n << 1);
+        n = a.size(); ST.resize(n << 1);
         for (int i = n; i < (n << 1); i ++)
             ST[i] = a[i - n];
         for (int i = n - 1; i > 0; i --)
@@ -16,21 +13,16 @@ struct IterativeSegmentTree{
         for (pos >>= 1; pos > 0; pos >>= 1)
             ST[pos] = merge(ST[pos << 1], ST[pos << 1 | 1]);
     }
-
     T query(int l, int r){
-        T ansL, ansR;
-        bool hasL = 0, hasR = 0;
-        r ++;
+        T ansL, ansR; bool hasL = 0, hasR = 0; r ++;
         for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
             if (l & 1) 
                 ansL = (hasL ? merge(ansL, ST[l ++]) : ST[l ++]), hasL = true;
             if (r & 1) 
                 ansR = (hasR ? merge(ST[ --r], ansR) : ST[ --r]), hasR = true;
         }
-        if (!hasL)
-            return ansR;
-        if (!hasR)
-            return ansL;
+        if (!hasL) return ansR;
+        if (!hasR) return ansL;
         return merge(ansL, ansR);
     }
 };
